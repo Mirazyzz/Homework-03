109680ec92b589658d5527cdcaa71de1b3222081
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-03-26 20:17:39 +0500

    Fibonacini boshlang'ich yechimi

--- Homework03/Homework03/Program.cs
+++ Homework03/Homework03/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int firstNumber, secondNumber, thirdNumber;
+
+            int seriesUntil = int.Parse(Console.ReadLine());
+
         }
     }
 }
